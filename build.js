3686454ca9e20d6374181cba1da34de1ad8eec13
Titus Wormer <tituswormer@gmail.com>
2018-08-28 23:11:38 +0200

    Update `syllable`

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
 var syllable = require('syllable');
@@ -548,415 +548,340 @@ module.exports={"105":"i","192":"A","193":"A","194":"A","195":"A","196":"A","197
 });
 
 },{}],5:[function(require,module,exports){
-'use strict';
+'use strict'
 
-var pluralize = require('pluralize');
-var normalize = require('normalize-strings');
-var problematic = require('./problematic.json');
+var pluralize = require('pluralize')
+var normalize = require('normalize-strings')
+var problematic = require('./problematic.json')
 
-module.exports = syllables;
+module.exports = syllables
 
-var own = {}.hasOwnProperty;
+var own = {}.hasOwnProperty
 
-/* Two expressions of occurrences which normally would
- * be counted as two syllables, but should be counted
- * as one. */
+// Two expressions of occurrences which normally would be counted as two
+// syllables, but should be counted as one.
 var EXPRESSION_MONOSYLLABIC_ONE = new RegExp(
-  'cia(l|$)|' +
-  'tia|' +
-  'cius|' +
-  'cious|' +
-  '[^aeiou]giu|' +
-  '[aeiouy][^aeiouy]ion|' +
-  'iou|' +
-  'sia$|' +
-  'eous$|' +
-  '[oa]gue$|' +
-  '.[^aeiuoycgltdb]{2,}ed$|' +
-  '.ely$|' +
-  '^jua|' +
-  'uai|' +
-  'eau|' +
-  '^busi$|' +
-  '(' +
-    '[aeiouy]' +
-    '(' +
-      'b|' +
-      'c|' +
-      'ch|' +
-      'dg|' +
-      'f|' +
-      'g|' +
-      'gh|' +
-      'gn|' +
-      'k|' +
-      'l|' +
-      'lch|' +
-      'll|' +
-      'lv|' +
-      'm|' +
-      'mm|' +
-      'n|' +
-      'nc|' +
-      'ng|' +
-      'nch|' +
-      'nn|' +
-      'p|' +
-      'r|' +
-      'rc|' +
-      'rn|' +
-      'rs|' +
-      'rv|' +
-      's|' +
-      'sc|' +
-      'sk|' +
-      'sl|' +
-      'squ|' +
-      'ss|' +
-      'th|' +
-      'v|' +
-      'y|' +
-      'z' +
-    ')' +
-    'ed$' +
-  ')|' +
-  '(' +
-    '[aeiouy]' +
-    '(' +
-      'b|' +
-      'ch|' +
-      'd|' +
-      'f|' +
-      'gh|' +
-      'gn|' +
-      'k|' +
-      'l|' +
-      'lch|' +
-      'll|' +
-      'lv|' +
-      'm|' +
-      'mm|' +
-      'n|' +
-      'nch|' +
-      'nn|' +
-      'p|' +
-      'r|' +
-      'rn|' +
-      'rs|' +
-      'rv|' +
-      's|' +
-      'sc|' +
-      'sk|' +
-      'sl|' +
-      'squ|' +
-      'ss|' +
-      'st|' +
-      't|' +
-      'th|' +
-      'v|' +
-      'y' +
-    ')' +
-    'es$' +
-  ')',
+  [
+    'cia(?:l|$)',
+    'tia',
+    'cius',
+    'cious',
+    '[^aeiou]giu',
+    '[aeiouy][^aeiouy]ion',
+    'iou',
+    'sia$',
+    'eous$',
+    '[oa]gue$',
+    '.[^aeiuoycgltdb]{2,}ed$',
+    '.ely$',
+    '^jua',
+    'uai',
+    'eau',
+    '^busi$',
+    '(?:[aeiouy](?:' +
+      [
+        '[bcfgklmnprsvwxyz]',
+        'ch',
+        'dg',
+        'g[hn]',
+        'lch',
+        'l[lv]',
+        'mm',
+        'nch',
+        'n[cgn]',
+        'r[bcnsv]',
+        'squ',
+        's[chkls]',
+        'th'
+      ].join('|') +
+      ')ed$)',
+    '(?:[aeiouy](?:' +
+      [
+        '[bdfklmnprstvy]',
+        'ch',
+        'g[hn]',
+        'lch',
+        'l[lv]',
+        'mm',
+        'nch',
+        'nn',
+        'r[nsv]',
+        'squ',
+        's[cklst]',
+        'th'
+      ].join('|') +
+      ')es$)'
+  ].join('|'),
   'g'
-);
+)
 
 var EXPRESSION_MONOSYLLABIC_TWO = new RegExp(
-  '[aeiouy]' +
-  '(' +
-    'b|' +
-    'c|' +
-    'ch|' +
-    'd|' +
-    'dg|' +
-    'f|' +
-    'g|' +
-    'gh|' +
-    'gn|' +
-    'k|' +
-    'l|' +
-    'll|' +
-    'lv|' +
-    'm|' +
-    'mm|' +
-    'n|' +
-    'nc|' +
-    'ng|' +
-    'nn|' +
-    'p|' +
-    'r|' +
-    'rc|' +
-    'rn|' +
-    'rs|' +
-    'rv|' +
-    's|' +
-    'sc|' +
-    'sk|' +
-    'sl|' +
-    'squ|' +
-    'ss|' +
-    'st|' +
-    't|' +
-    'th|' +
-    'v|' +
-    'y|' +
-    'z' +
-  ')' +
-  'e$',
+  '[aeiouy](?:' +
+    [
+      '[bcdfgklmnprstvyz]',
+      'ch',
+      'dg',
+      'g[hn]',
+      'l[lv]',
+      'mm',
+      'n[cgn]',
+      'r[cnsv]',
+      'squ',
+      's[cklst]',
+      'th'
+    ].join('|') +
+    ')e$',
   'g'
-);
+)
 
-/* Four expression of occurrences which normally would be
- * counted as one syllable, but should be counted as two. */
+// Four expression of occurrences which normally would be counted as one
+// syllable, but should be counted as two.
 var EXPRESSION_DOUBLE_SYLLABIC_ONE = new RegExp(
-  '(' +
-    '(' +
-      '[^aeiouy]' +
-    ')\\2l|' +
-    '[^aeiouy]ie' +
-    '(' +
-      'r|' +
-      'st|' +
-      't' +
-    ')|' +
-    '[aeiouym]bl|' +
-    'eo|' +
-    'ism|' +
-    'asm|' +
-    'thm|' +
-    'dnt|' +
-    'uity|' +
-    'dea|' +
-    'gean|' +
-    'oa|' +
-    'ua|' +
-    'eings?|' +
-    '[aeiouy]sh?e[rsd]' +
-  ')$',
+  '(?:' +
+    [
+      '([^aeiouy])\\1l',
+      '[^aeiouy]ie(?:r|s?t)',
+      '[aeiouym]bl',
+      'eo',
+      'ism',
+      'asm',
+      'thm',
+      'dnt',
+      'snt',
+      'uity',
+      'dea',
+      'gean',
+      'oa',
+      'ua',
+      'react?',
+      'orbed', // Cancel `'.[^aeiuoycgltdb]{2,}ed$',`
+      'eings?',
+      '[aeiouy]sh?e[rs]'
+    ].join('|') +
+    ')$',
   'g'
-);
+)
 
 var EXPRESSION_DOUBLE_SYLLABIC_TWO = new RegExp(
-  '[^gq]ua[^auieo]|' +
-  '[aeiou]{3}|' +
-  '^(' +
-    'ia|' +
-    'mc|' +
-    'coa[dglx].' +
-  ')',
+  [
+    '[^gq]ua[^auieo]',
+    '[aeiou]{3}',
+    '^(?:ia|mc|coa[dglx].)',
+    '^re(app|es|im|us)'
+  ].join('|'),
   'g'
-);
+)
 
 var EXPRESSION_DOUBLE_SYLLABIC_THREE = new RegExp(
-  '[^aeiou]y[ae]|' +
-  '[^l]lien|' +
-  'riet|' +
-  'dien|' +
-  'iu|' +
-  'io|' +
-  'ii|' +
-  'uen|' +
-  'real|' +
-  'iell|' +
-  'eo[^aeiou]|' +
-  '[aeiou]y[aeiou]',
+  [
+    '[^aeiou]y[ae]',
+    '[^l]lien',
+    'riet',
+    'dien',
+    'iu',
+    'io',
+    'ii',
+    'uen',
+    'real',
+    'iell',
+    'eo[^aeiou]',
+    '[aeiou]y[aeiou]'
+  ].join('|'),
   'g'
-);
+)
 
-var EXPRESSION_DOUBLE_SYLLABIC_FOUR = /[^s]ia/;
+var EXPRESSION_DOUBLE_SYLLABIC_FOUR = /[^s]ia/
 
-/* Expression to match single syllable pre- and suffixes. */
+// Expression to match single syllable pre- and suffixes.
 var EXPRESSION_SINGLE = new RegExp(
-  '^' +
-  '(' +
-    'un|' +
-    'fore|' +
-    'ware|' +
-    'none?|' +
-    'out|' +
-    'post|' +
-    'sub|' +
-    'pre|' +
-    'pro|' +
-    'dis|' +
-    'side' +
-  ')' +
-  '|' +
-  '(' +
-    'ly|' +
-    'less|' +
-    'some|' +
-    'ful|' +
-    'ers?|' +
-    'ness|' +
-    'cians?|' +
-    'ments?|' +
-    'ettes?|' +
-    'villes?|' +
-    'ships?|' +
-    'sides?|' +
-    'ports?|' +
-    'shires?|' +
-    'tion(ed)?' +
-  ')' +
-  '$',
+  [
+    '^(?:' +
+      [
+        'un',
+        'fore',
+        'ware',
+        'none?',
+        'out',
+        'post',
+        'sub',
+        'pre',
+        'pro',
+        'dis',
+        'side'
+      ].join('|') +
+      ')',
+    '(?:' +
+      [
+        'ly',
+        'less',
+        'some',
+        'ful',
+        'ers?',
+        'ness',
+        'cians?',
+        'ments?',
+        'ettes?',
+        'villes?',
+        'ships?',
+        'sides?',
+        'ports?',
+        'shires?',
+        'tion(?:ed|s)?'
+      ].join('|') +
+      ')$'
+  ].join('|'),
   'g'
-);
+)
 
-/* Expression to match double syllable pre- and suffixes. */
+// Expression to match double syllable pre- and suffixes.
 var EXPRESSION_DOUBLE = new RegExp(
-  '^' +
-  '(' +
-    'above|' +
-    'anti|' +
-    'ante|' +
-    'counter|' +
-    'hyper|' +
-    'afore|' +
-    'agri|' +
-    'infra|' +
-    'intra|' +
-    'inter|' +
-    'over|' +
-    'semi|' +
-    'ultra|' +
-    'under|' +
-    'extra|' +
-    'dia|' +
-    'micro|' +
-    'mega|' +
-    'kilo|' +
-    'pico|' +
-    'nano|' +
-    'macro' +
-  ')' +
-  '|' +
-  '(' +
-    'fully|' +
-    'berry|' +
-    'woman|' +
-    'women' +
-  ')' +
-  '$',
+  [
+    '^' +
+      '(?:' +
+      [
+        'above',
+        'anti',
+        'ante',
+        'counter',
+        'hyper',
+        'afore',
+        'agri',
+        'infra',
+        'intra',
+        'inter',
+        'over',
+        'semi',
+        'ultra',
+        'under',
+        'extra',
+        'dia',
+        'micro',
+        'mega',
+        'kilo',
+        'pico',
+        'nano',
+        'macro'
+      ].join('|') +
+      ')',
+    '(?:' + ['fully', 'berry', 'woman', 'women', 'edly'].join('|') + ')$'
+  ].join('|'),
   'g'
-);
+)
 
-/* Expression to match triple syllable suffixes. */
-var EXPRESSION_TRIPLE = /(ology|ologist|onomy|onomist)$/g;
+// Expression to match triple syllable suffixes.
+var EXPRESSION_TRIPLE = /(ology|ologist|onomy|onomist)$/g
 
-/* Expression to split on word boundaries. */
-var SPLIT = /\b/g;
+// Expression to split on word boundaries.
+var SPLIT = /\b/g
 
-/* Expression to remove non-alphabetic characters from
- * a given value. */
-var EXPRESSION_NONALPHABETIC = /[^a-z]/g;
+// Expression to merge elision.
+var APOSTROPHE = /['’]/g
 
-/* Wrapper to support multiple word-parts (GH-11). */
+// Expression to remove non-alphabetic characters from a given value.
+var EXPRESSION_NONALPHABETIC = /[^a-z]/g
+
+// Wrapper to support multiple word-parts (GH-11).
 function syllables(value) {
-  var values = normalize(String(value)).toLowerCase().split(SPLIT);
-  var length = values.length;
-  var index = -1;
-  var total = 0;
+  var values = normalize(String(value))
+    .toLowerCase()
+    .replace(APOSTROPHE, '')
+    .split(SPLIT)
+  var length = values.length
+  var index = -1
+  var total = 0
 
   while (++index < length) {
-    total += syllable(values[index].replace(EXPRESSION_NONALPHABETIC, ''));
+    total += syllable(values[index].replace(EXPRESSION_NONALPHABETIC, ''))
   }
 
-  return total;
+  return total
 }
 
-/* Get syllables in a given value. */
+// Get syllables in a given value.
 function syllable(value) {
-  var count = 0;
-  var index;
-  var length;
-  var singular;
-  var parts;
-  var addOne;
-  var subtractOne;
+  var count = 0
+  var index
+  var length
+  var singular
+  var parts
+  var addOne
+  var subtractOne
 
   if (value.length === 0) {
-    return count;
+    return count
   }
 
-  /* Return early when possible. */
+  // Return early when possible.
   if (value.length < 3) {
-    return 1;
+    return 1
   }
 
-  /* If `value` is a hard to count, it might be
-   * in `problematic`. */
+  // If `value` is a hard to count, it might be in `problematic`.
   if (own.call(problematic, value)) {
-    return problematic[value];
+    return problematic[value]
   }
 
-  /* Additionally, the singular word might be
-   * in `problematic`. */
-  singular = pluralize(value, 1);
+  // Additionally, the singular word might be in `problematic`.
+  singular = pluralize(value, 1)
 
   if (own.call(problematic, singular)) {
-    return problematic[singular];
+    return problematic[singular]
   }
 
-  addOne = returnFactory(1);
-  subtractOne = returnFactory(-1);
+  addOne = returnFactory(1)
+  subtractOne = returnFactory(-1)
 
-  /* Count some prefixes and suffixes, and remove
-   * their matched ranges. */
+  // Count some prefixes and suffixes, and remove their matched ranges.
   value = value
     .replace(EXPRESSION_TRIPLE, countFactory(3))
     .replace(EXPRESSION_DOUBLE, countFactory(2))
-    .replace(EXPRESSION_SINGLE, countFactory(1));
+    .replace(EXPRESSION_SINGLE, countFactory(1))
 
-  /* Count multiple consonants. */
-  parts = value.split(/[^aeiouy]+/);
-  index = -1;
-  length = parts.length;
+  // Count multiple consonants.
+  parts = value.split(/[^aeiouy]+/)
+  index = -1
+  length = parts.length
 
   while (++index < length) {
     if (parts[index] !== '') {
-      count++;
+      count++
     }
   }
 
-  /* Subtract one for occurrences which should be
-   * counted as one (but are counted as two). */
+  // Subtract one for occurrences which should be counted as one (but are
+  // counted as two).
   value
     .replace(EXPRESSION_MONOSYLLABIC_ONE, subtractOne)
-    .replace(EXPRESSION_MONOSYLLABIC_TWO, subtractOne);
+    .replace(EXPRESSION_MONOSYLLABIC_TWO, subtractOne)
 
-  /* Add one for occurrences which should be counted
-   * as two (but are counted as one). */
+  // Add one for occurrences which should be counted as two (but are counted as
+  // one).
   value
     .replace(EXPRESSION_DOUBLE_SYLLABIC_ONE, addOne)
     .replace(EXPRESSION_DOUBLE_SYLLABIC_TWO, addOne)
     .replace(EXPRESSION_DOUBLE_SYLLABIC_THREE, addOne)
-    .replace(EXPRESSION_DOUBLE_SYLLABIC_FOUR, addOne);
+    .replace(EXPRESSION_DOUBLE_SYLLABIC_FOUR, addOne)
 
-  /* Make sure at least on is returned. */
-  return count || 1;
+  // Make sure at least on is returned.
+  return count || 1
 
- /* Define scoped counters, to be used
-  * in `String#replace()` calls.
-  * The scoped counter removes the matched value
-  * from the input. */
+  // Define scoped counters, to be used in `String#replace()` calls.
+  // The scoped counter removes the matched value from the input.
   function countFactory(addition) {
-    return counter;
+    return counter
     function counter() {
-      count += addition;
-      return '';
+      count += addition
+      return ''
     }
   }
 
- /* Define scoped counters, to be used
-  * in `String#replace()` calls.
-  * The scoped counter does not remove the matched
-  * value from the input. */
+  // Define scoped counters, to be used in `String#replace()` calls.
+  // The scoped counter does not remove the matched value from the input.
   function returnFactory(addition) {
-    return returner;
+    return returner
     function returner($0) {
-      count += addition;
-      return $0;
+      count += addition
+      return $0
     }
   }
 }
@@ -995,8 +920,10 @@ module.exports={
   "karate": 3,
   "machete": 3,
   "maybe": 2,
+  "newlywed": 3,
   "people": 2,
   "recipe": 3,
+  "riverbed": 3,
   "sesame": 3,
   "shoreline": 2,
   "simile": 3,
@@ -1010,6 +937,7 @@ module.exports={
   "penelope": 4,
   "persephone": 4,
   "phoebe": 2,
+  "waterbed": 3,
   "zoe": 2
 }
 
